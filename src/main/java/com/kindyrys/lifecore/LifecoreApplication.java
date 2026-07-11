5f0a985d478d451858fd220d96d640efcf07084b
KMB <kindyrys@gmail.com>
2026-07-11 22:22:54 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/com/kindyrys/lifecore/LifecoreApplication.java
@@ -0,0 +1,13 @@
+package com.kindyrys.lifecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifecoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifecoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kindyrys/lifecore/LifecoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kindyrys.lifecore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifecoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
